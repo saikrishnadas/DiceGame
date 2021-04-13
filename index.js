c3acf28dc32505c16ccdfddec0565952d0b03d85
Sai <saikrishnadas666@gmail.com>
2021-04-13 12:00:24 +0530

    document model error

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+//player1
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomImage1 = "images/"+"dice"+randomNumber1+".png";
 
-var img1 = document.querySelectorAll("img")[0];
-img1.setAttribute("src",randomImage1);
+document.querySelectorAll("img")[0].setAttribute("src",randomImage1);
 
+//player2
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImage2 = "images/"+"dice"+randomNumber2+".png";
 
-var img2 = document.querySelectorAll("img")[1];
-img2.setAttribute("src",randomImage2);
+document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
+
 
 //condition
 if (randomNumber1 > randomNumber2) {
