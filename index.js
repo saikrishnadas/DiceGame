610eebd4d82036302643ee6f25f0acb6cb308321
Sai <saikrishnadas666@gmail.com>
2021-04-13 13:08:41 +0530

    final push

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
 
 //condition
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins";
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
 } else if (randomNumber1 < randomNumber2){
     document.querySelector("h1").innerHTML = "Player 2 Wins";
 } else {
